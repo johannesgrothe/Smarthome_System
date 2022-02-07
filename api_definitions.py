4dd01583dc6ae32769823391bbef6c3d76589460
Johannes Grothe <johannesgrothe@web.de>
2022-02-08 00:24:54 +0100

    #54 Adapting exporter

--- api_definitions.py
+++ api_definitions.py
@@ -13,7 +13,7 @@ except ModuleNotFoundError:
 
 
 # API Version
-api_version = SoftwareVersion(1, 0, 5)
+api_version = SoftwareVersion(1, 0, 6)
 
 
 class ApiURIs(StringSystemIdentifier):
--- exporters/api_doc_exporter.py
+++ exporters/api_doc_exporter.py
@@ -32,6 +32,52 @@ class ApiDocExporter(DocExporter):
         else:
             return ", ".join(data)
 
+    @staticmethod
+    def _format_sender(sender: str) -> str:
+        switcher =  {
+            "bridge": "Bridge",
+            "client": "Client",
+            "web_application": "Web Application"
+        }
+        return switcher.get(sender)
+
+    def _export_mapping(self, map_data: dict, file: MarkdownFile, receiver: str):
+        file.add(MarkdownHeader(map_data["title"], 2))
+        file.add(MarkdownText(map_data["description"]))
+
+        status_table = MarkdownTable(["Option", "Value"])
+        status_table.add_line(["Broadcast Allowed", ('Yes' if map_data['broadcast_allowed'] else 'No')])
+        status_table.add_line(["Sender", self._format_list([self._format_sender(x) for x in map_data['sender']],
+                                                           "Anybody")])
+        status_table.add_line(["Receiver", self._format_sender(receiver)])
+
+        try:
+            http_method = "No HTTP Allowed" if not map_data['http_method'] else map_data['http_method']
+            status_table.add_line(["HTTP Method", http_method])
+        except KeyError:
+            pass
+
+        file.add(status_table)
+
+        file.add(MarkdownCode(map_data["uri"]["value"], language="json", block=False))
+
+        file.add(MarkdownHeader("Request", 3))
+        req_data = map_data["request"]
+        file.add(MarkdownText(req_data["comment"]))
+        file.add(MarkdownHyperLink(req_data['schema'], f"{_schema_link_base_path}{req_data['schema']}", "Schema: "))
+        req_schema = self._read_schema(req_data["schema"])
+        file.add(MarkdownCode(req_schema, language="json"))
+
+        file.add(MarkdownHeader("Response", 3))
+        res_data = map_data["response"]
+        file.add(MarkdownText(res_data["comment"]))
+        if res_data["schema"] is not None:
+            file.add(
+                MarkdownHyperLink(res_data['schema'], f"{_schema_link_base_path}{res_data['schema']}", "Schema: "))
+            res_schema = self._read_schema(res_data["schema"])
+            file.add(MarkdownCode(res_schema, language="json"))
+        file.add(MarkdownDivider())
+
     def export_docs(self, out_file: str):
         file = MarkdownFile()
         file.add(MarkdownHeader(self._definitions["title"], 0))
@@ -42,44 +88,12 @@ class ApiDocExporter(DocExporter):
         self._add_exported_libraries(PATH_FILE_API_CONSTANTS, file)
         file.add(MarkdownDivider())
 
-        file.add(MarkdownHeader("Table of Contents", 1))
-        for mapping in self._definitions["mappings"]:
-            map_data = self._definitions["mappings"][mapping]
-            target: str = map_data["title"].lower().replace(" ", "-")
-            file.add(MarkdownInternalLink(map_data["title"], target))
-        file.add(MarkdownDivider())
+        file.add(MarkdownHeader("Bridge", 1))
+        for _, map_data in self._definitions["mappings"]["bridge"].items():
+            self._export_mapping(map_data, file, "bridge")
 
-        for mapping in self._definitions["mappings"]:
-            map_data = self._definitions["mappings"][mapping]
-            file.add(MarkdownHeader(map_data["title"], 2))
-            file.add(MarkdownText(map_data["description"]))
-
-            status_table = MarkdownTable(["Option", "Value"])
-            status_table.add_line(["Broadcast Allowed", ('Yes' if map_data['broadcast_allowed'] else 'No')])
-            status_table.add_line(["Sender", self._format_list(map_data['sender'], "Anybody")])
-            status_table.add_line(["Receiver", self._format_list(map_data['receiver'], "Anybody")])
-            http_method = "No HTTP Allowed" if not map_data['http_method'] else map_data['http_method']
-            status_table.add_line(["HTTP Method", http_method])
-
-            file.add(status_table)
-
-            file.add(MarkdownCode(map_data["uri"]["value"], language="json", block=False))
-
-            file.add(MarkdownHeader("Request", 3))
-            req_data = map_data["request"]
-            file.add(MarkdownText(req_data["comment"]))
-            file.add(MarkdownHyperLink(req_data['schema'], f"{_schema_link_base_path}{req_data['schema']}", "Schema: "))
-            req_schema = self._read_schema(req_data["schema"])
-            file.add(MarkdownCode(req_schema, language="json"))
-
-            file.add(MarkdownHeader("Response", 3))
-            res_data = map_data["response"]
-            file.add(MarkdownText(res_data["comment"]))
-            if res_data["schema"] is not None:
-                file.add(
-                    MarkdownHyperLink(res_data['schema'], f"{_schema_link_base_path}{res_data['schema']}", "Schema: "))
-                res_schema = self._read_schema(res_data["schema"])
-                file.add(MarkdownCode(res_schema, language="json"))
-            file.add(MarkdownDivider())
+        file.add(MarkdownHeader("Client", 1))
+        for _, map_data in self._definitions["mappings"]["client"].items():
+            self._export_mapping(map_data, file, "client")
 
         file.save(out_file)
