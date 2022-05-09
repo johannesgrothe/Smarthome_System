aa80d378a5224e88a198a2fd55d4769cd259defa
Johannes Grothe <johannesgrothe@web.de>
2022-05-09 21:07:56 +0200

    #71 Adding direction-attribute for bridge to api doc

--- api_definitions.py
+++ api_definitions.py
@@ -35,104 +35,121 @@ class ApiURIs(ApiURIsSuper):
     # Write Complete Config to Client
     client_config_write = ApiEndpointDefinition("config/write",
                                                 [ApiAccessLevel.admin],
-                                                ApiAccessType.write)
+                                                ApiAccessType.write,
+                                                False)
 
     # Delete Config
     client_config_delete = ApiEndpointDefinition("config/delete",
                                                  [ApiAccessLevel.admin],
-                                                 ApiAccessType.write)
+                                                 ApiAccessType.write,
+                                                 False)
 
     # Client Heartbeat
     heartbeat = ApiEndpointDefinition("heartbeat",
                                       [ApiAccessLevel.admin, ApiAccessLevel.mqtt],
-                                      ApiAccessType.write)
+                                      ApiAccessType.write,
+                                      False)
 
     # Read Bridge Info
     info_bridge = ApiEndpointDefinition("info/bridge",
                                         [ApiAccessLevel.admin, ApiAccessLevel.user, ApiAccessLevel.guest],
-                                        ApiAccessType.read)
+                                        ApiAccessType.read,
+                                        False)
 
     # Read Clients Info
     info_clients = ApiEndpointDefinition("info/clients",
                                          [ApiAccessLevel.admin, ApiAccessLevel.user, ApiAccessLevel.guest],
-                                         ApiAccessType.read)
+                                         ApiAccessType.read,
+                                         False)
 
     # Read Gadgets Info
     info_gadgets = ApiEndpointDefinition("info/gadgets",
                                          [ApiAccessLevel.admin, ApiAccessLevel.user, ApiAccessLevel.guest],
-                                         ApiAccessType.read)
+                                         ApiAccessType.read,
+                                         False)
 
     # Update Gadget
     update_gadget = ApiEndpointDefinition("update/gadget",
                                           [ApiAccessLevel.admin, ApiAccessLevel.mqtt, ApiAccessLevel.user],
-                                          ApiAccessType.write)
+                                          ApiAccessType.write,
+                                          False)
 
     # Sync Client
     sync_client = ApiEndpointDefinition("sync/client",
                                         [ApiAccessLevel.admin, ApiAccessLevel.mqtt],
-                                        ApiAccessType.write)
+                                        ApiAccessType.write,
+                                        False)
 
     # Reboot Client
     reboot_connected_client = ApiEndpointDefinition("reboot/client",
                                                     [ApiAccessLevel.admin, ApiAccessLevel.user],
-                                                    ApiAccessType.write)
+                                                    ApiAccessType.write,
+                                                    False)
 
     # Sync Event
     sync_event = ApiEndpointDefinition("sync/event",
                                        [ApiAccessLevel.admin, ApiAccessLevel.mqtt],
-                                       ApiAccessType.write)
+                                       ApiAccessType.write,
+                                       False)
 
     # Test Echo
     test_echo = ApiEndpointDefinition("echo",
                                       [ApiAccessLevel.admin],
-                                      ApiAccessType.write)
+                                      ApiAccessType.write,
+                                      False)
 
     # Retrieve all stored configs
     config_storage_get_all = ApiEndpointDefinition("config/storage/get_all",
                                                    [ApiAccessLevel.admin, ApiAccessLevel.user],
-                                                   ApiAccessType.read)
+                                                   ApiAccessType.read,
+                                                   False)
 
     # Retrieve stored config
     config_storage_get = ApiEndpointDefinition("config/storage/get",
                                                [ApiAccessLevel.admin, ApiAccessLevel.user],
-                                               ApiAccessType.read)
+                                               ApiAccessType.read,
+                                               False)
 
     # Save config
     config_storage_save = ApiEndpointDefinition("config/storage/save",
                                                 [ApiAccessLevel.admin],
-                                                ApiAccessType.write)
+                                                ApiAccessType.write,
+                                                False)
 
     # Delete Config
     config_storage_delete = ApiEndpointDefinition("config/storage/delete",
                                                   [ApiAccessLevel.admin],
-                                                  ApiAccessType.write)
+                                                  ApiAccessType.write,
+                                                  False)
 
     # Bridge update check
     bridge_update_check = ApiEndpointDefinition("bridge/update/check",
                                                 [ApiAccessLevel.admin],
-                                                ApiAccessType.read)
+                                                ApiAccessType.read,
+                                                False)
 
     # Bridge update execute
     bridge_update_execute = ApiEndpointDefinition("bridge/update/execute",
                                                   [ApiAccessLevel.admin],
-                                                  ApiAccessType.write)
+                                                  ApiAccessType.write,
+                                                  False)
 
     # URIs exposed by the client
 
     # Write System Config
-    client_system_config_write = ApiEndpointDefinition("config/system/write", [], None)
+    client_system_config_write = ApiEndpointDefinition("config/system/write", [], None, True)
 
     # Write Event Config
-    client_event_config_write = ApiEndpointDefinition("config/event/write", [], None)
+    client_event_config_write = ApiEndpointDefinition("config/event/write", [], None, True)
 
     # Write Gadget Config
-    client_gadget_config_write = ApiEndpointDefinition("config/gadget/write", [], None)
+    client_gadget_config_write = ApiEndpointDefinition("config/gadget/write", [], None, True)
 
     # Reboot Client
-    client_reboot = ApiEndpointDefinition("reboot/client", [], None)
+    client_reboot = ApiEndpointDefinition("reboot/client", [], None, True)
 
     # Client Sync Request
-    sync_request = ApiEndpointDefinition("sync", [], None)
+    sync_request = ApiEndpointDefinition("sync", [], None, True)
 
     # Sync Event
-    client_sync_event = ApiEndpointDefinition("sync/event", [], None)
+    client_sync_event = ApiEndpointDefinition("sync/event", [], None, True)
--- utils/api_endpoint_definition.py
+++ utils/api_endpoint_definition.py
@@ -12,24 +12,43 @@ class ApiAccessType(enum.IntEnum):
 
 
 class ApiEndpointDefinition:
-    uri: str
-    access_levels: list[ApiAccessLevelSuper]
-    access_type: Optional[ApiAccessType]
+    _uri: str
+    _access_levels: list[ApiAccessLevelSuper]
+    _access_type: Optional[ApiAccessType]
+    _outgoing: bool
 
-    def __init__(self, uri: str, access_levels: list[ApiAccessLevelSuper], access_type: Optional[ApiAccessType]):
-        self.uri = uri
-        self.access_levels = access_levels
-        self.access_type = access_type
+    def __init__(self, uri: str, access_levels: list[ApiAccessLevelSuper], access_type: Optional[ApiAccessType],
+                 outgoing: bool):
+        self._uri = uri
+        self._access_levels = access_levels
+        self._access_type = access_type
+        self._outgoing = outgoing
+
+    @property
+    def uri(self) -> str:
+        return self._uri
+
+    @property
+    def access_levels(self) -> list[ApiAccessLevelSuper]:
+        return self._access_levels
+
+    @property
+    def access_type(self) -> Optional[ApiAccessType]:
+        return self._access_type
+
+    @property
+    def outgoing(self) -> bool:
+        return self._outgoing
 
 
 class ApiURIsSuper:
     @classmethod
-    def _get_endpoints(cls) -> list[ApiEndpointDefinition]:
+    def get_endpoints(cls) -> list[ApiEndpointDefinition]:
         return [getattr(cls, x) for x in cls.__dict__.keys() if isinstance(getattr(cls, x), ApiEndpointDefinition)]
 
     @classmethod
     def get_definition_for_uri(cls, value: str):
-        for endpoint in cls._get_endpoints():
+        for endpoint in cls.get_endpoints():
             if endpoint.uri == value:
                 return endpoint
         raise ValueError(value)
